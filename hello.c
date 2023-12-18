ae820070669159b2c9d3e4baa0979ce3bf065e1f
Chris Garrett <chris@chrisg.com>
2023-12-18 09:11:06 +0000

    ifdef

--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <conio.h>
-#include <cbm.h>
-#include <peekpoke.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <conio.h>
+
+#ifdef __CBM__
+    #include <cbm.h>
+    #include <peekpoke.h>
+#endif
 
 // Global key variable
 bool run=true;
@@ -375,8 +378,10 @@ int main() {
     /* Clear Screen */
     clrscr();
 
+#ifdef __PET__
     // Uppercase/Graphical characterset = 12
     POKE(59468,12);
+#endif
 
     /* Hide cursor */
     cursor(0);
