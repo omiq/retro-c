172a1b886ba2bbd9543510d6b82d01b41a4c7b94
omiq <chris@chrisg.com>
2025-02-16 17:38:06 +0000

    conio.h compatibility test

--- cpm-conio.c
+++ cpm-conio.c
@@ -1,13 +1,50 @@
+/*
+	Requires console compatiblity 
+	zcc +cpm -o cpm-conio.com -subtype=z80pack -create-app cpm-conio.c --generic-console -pragma-define:CONSOLE_COLUMNS=80 -pragma-define:CONSOLE_ROWS=24
+*/
 #include <stdio.h>
 #include <conio.h>
-
-void main() {
-
-
-	while(1) {
-		printf("%c",getch());
-	}
-
-
-
+#include <ctype.h>
+// Global key variable
+int key;
+char x,y=10;
+int main()
+{
+	/* Clear Screen */
+	clrscr();
+    /* Loop until Q is pressed */
+    while ((key  = toupper(cgetc())) != 'Q')
+    {
+        // Delete the character
+        gotoxy(x,y);
+        putch('.');
+        // keys;
+        switch (key) 
+        { 
+            case 'W': 
+            case 'w': 
+                y--; 
+                break; 
+            case 'A':
+            case 'a':  
+                x--; 
+                break; 
+            case 'S': 
+            case 's': 
+                y++; 
+                break; 
+            case 'D':
+            case 'd': 
+                x++; 
+                break; 
+            default: 
+                break; 
+        }
+        gotoxy(x,y);
+        putch('@');
+    
+    }
+    //cursor(1);
+    return(0);
 }
+
