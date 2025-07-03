968613b01bc9884493fee5ecf28c7d5df6addb86
Chris Garrett <chris@chrisg.com>
2025-07-03 19:13:39 +0100

    restart tutorials

--- maze.c
+++ maze.c
@@ -1,23 +1,26 @@
 #include "maze.h"
-
+#include <conio.h>
+#include <cc65.h>
 
 int main(void) {
     int i;
     srand((unsigned)time(NULL));
+    clrscr();
+
     carveMaze();
     placePlayer();
 
     /* Print top HUD line */
-    //printf("HUD: Score 0\n");
+    printf("HUD: Score 0\n");
 
     /* Print the playable area */
-    for (i = HUD_TOP; i < MAP_HEIGHT - HUD_BOTTOM; i++) {
-        printf("%s", map[i]);
+    for (i = 0; i < 22; i++) {
+        printf("%s\n", map[i]);
     }
 
     /* Print bottom HUD lines */
-    //printf("Status: OK\n");
-    //printf("Commands: WASD to move\n");
-
+    printf("Status: OK\n");
+    printf("Commands: WASD to move");
+    i=getchar();
     return 0;
 }
